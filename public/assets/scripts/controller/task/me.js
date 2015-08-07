732b3455e4129221ccfbe1c7642db0f464685bca
IndexXuan <pengruihaha@163.com>
2015-08-07 18:47:51 +0800

    final 20150806 & finished me

--- public/assets/scripts/controller/task/me.js
+++ public/assets/scripts/controller/task/me.js
@@ -9,31 +9,40 @@ define([], function() {
 
     // prefix of localStorage
     //var cachedPrefix = 'illy-task-article-';
-    
+
     // prefix of resource
     var resourcePrefix = 'http://resource.hizuoye.com/';
-    
+
     // avatar manage
     var avatar = {
+        defaultFullUrl: '',
         localId: '',
         serverId: ''
     }
-    
+
     var me = avalon.define({
         $id: "me",
-        //hasEdited: false,
+        infoProfile: ['displayName', 'gender', 'phone', 'parent', 'onSchool', 'grade'],
         editing: false,
-        copyOfJSON: '',
+        copyProfile: '',
         username: '',
         displayName: '',
         gender: '',
         phone: '',
-        parend: '',
+        parent: '',
         avatar: '',
         onSchool: '',
         grade: '',
         finishedHomeworkCount: '',
         finishedPreviewsCount: '',
+        //infoCollect: {
+        //    'displayName': '',
+        //    'gender': '',
+        //    'phone': '',
+        //    'parent': '',
+        //    'onSchool': '',
+        //    'grade': ''
+        //},
         fetchData: function() {
             //if (me.visited) {
             //    return; // core!!! key!!! forget this will getCache and request!!!
@@ -45,59 +54,103 @@ define([], function() {
                 },
                 dataType: "json",
                 success: function(json) {
-                    me.copyOfJSON = json;
-                    me.setVM(json);
+                    me.copyProfile = json;
+                    me.setVM(json, true);
                 }
             })
-       },
-       chooseImage: function() {
-           alert(1)
-           wx.chooseImage({
-               count: 1, // 默认9
-               sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-               sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-               success: function (res) {
-                   var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                   avatar.localId = localIds && localIds[0];
-                   //alert('choose ' + localIds.length + ' images!');
-                   me.uploadImage();
-                   me.avatar = avatar.localId; // change it now 
-               }
-           });
-       },
-       uploadImage: function() {
-           wx.uploadImage({
-               localId: avatar.localId, // 需要上传的图片的本地ID，由chooseImage接口获得
-               isShowProgressTips: 1, // 默认为1，显示进度提示
-               success: function (res) {
-                   var serverId = res.serverId; // 返回图片的服务器端ID
-                   avatar.serverId = serverId;
-               }
-           });
-       },
-       toggleEditState: function() {
-           me.hasEdited = true;
-           if (me.editing === true) {
-               me.editing = false;
-           } else {
-               me.editing = true;
-           }
-       },
-       setVM: function(source) {
-           me.username = source.username;
-           me.displayName = source.displayName;
-           me.gender = source.gender;
-           me.phone = source.phone;
-           me.parend = source.parent;
-           me.avatar = resourcePrefix + source.avatar;
-           me.onSchool = source.onSchool;
-           me.grade = source.grade;
-           me.finishedHomeworkCount = source.finishedHomeworkCount;
-           me.finishedPreviewsCount = source.finishedPreviewsCount;
-       },
-       resetAll: function() {
-           me.setVM(me.copyOfJSON);
-       }
+        },
+        chooseImage: function() {
+            wx.chooseImage({
+                count: 1, // 默认9
+                sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    avatar.localId = localIds && localIds[0];
+                    //alert('choose ' + localIds.length + ' images!');
+                    me.uploadImage();
+                    me.avatar = avatar.localId; // change it now 
+                }
+            });
+        },
+        uploadImage: function() {
+            wx.uploadImage({
+                localId: avatar.localId, // 需要上传的图片的本地ID，由chooseImage接口获得
+                isShowProgressTips: 1, // 默认为1，显示进度提示
+                success: function (res) {
+                    var serverId = res.serverId; // 返回图片的服务器端ID
+                    avatar.serverId = serverId;
+                }
+            });
+        },
+        toggleEditState: function() {
+            if (me.editing === true) {
+                me.editing = false;
+            } else {
+                me.editing = true;
+            }
+        },
+        setVM: function(source, avatar) {
+            if (avatar != false) avatar = true; // default
+            me.username = source.username;
+            me.displayName = source.displayName;
+            me.gender = source.gender;
+            me.phone = source.phone;
+            me.parent = source.parent;
+            if (avatar) {
+                if (source.avatar != void 0) {
+                    me.avatar = resourcePrefix + source.avatar;
+                } else {
+                    me.avatar = avatar.defaultFullUrl; // default avatar of user
+                }
+            }
+            me.onSchool = source.onSchool;
+            me.grade = source.grade;
+            me.finishedHomeworkCount = source.finishedHomeworkCount;
+            me.finishedPreviewsCount = source.finishedPreviewsCount;
+        },
+        resetAll: function() {
+            me.setVM(me.copyProfile, false);
+        },
+        hasDiff: function() {
+            var diff = me.infoProfile.every(function(item) {
+                return me[item] === me.copyProfile[item];
+            })
+            return !diff;
+        },
+        updateProfile: function() {
+            $http.ajax({
+                method: 'PUT',
+                url: apiBaseUrl + 'profile',
+                headers: {
+                    Authorization: 'Bearer ' + token
+                },
+                data: {
+                    displayName: me.displayName,
+                    gender: me.gender,
+                    phone: me.phone,
+                    parent: me.parent,
+                    onSchool: me.onSchool,
+                    grade: me.grade
+                },
+                success: function(res) {
+
+                },
+                error: function(res) {
+                    alert('对不起，账户信息更新失败...');
+                    me.resetAll(); // 回滚页面ui中账户信息，做到更新失败的回滚
+                },
+                ajaxFail: function(res) {
+                    alert('对不起，账户信息更新失败...');
+                    me.resetAll(); // 回滚页面ui中账户信息，做到更新失败的回滚
+                }
+            })
+        },
+        save: function() { // diff will update and no-diff will just local save
+            if(me.hasDiff()) {
+                me.updateProfile();
+            }
+        }
     });
 
     return avalon.controller(function($ctrl) {
@@ -107,9 +160,9 @@ define([], function() {
         }
         // 进入视图
         $ctrl.$onEnter = function(params) {
-
+            avatar.localId = '';
+            avatar.serverId = '';
             me.fetchData();
-
         }
         // 对应的视图销毁前
         $ctrl.$onBeforeUnload = function() {
