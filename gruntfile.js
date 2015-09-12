fc901bc36b4a201db272d25ab7ceaf6c415c6ef7
IndexXuan <pengruihaha@163.com>
2015-09-12 18:35:22 +0800

    new full build system & global_config & fix bug & add new grunt task

--- gruntfile.js
+++ gruntfile.js
@@ -198,23 +198,11 @@ module.exports = function(grunt) { /* jshint ignore:line */
                 },
                 files: [{
                     expand: true,
-                    cwd: 'public/assets/scripts', //js目录下
+                    cwd: 'public/assets/scripts/', //js目录下
                     src: '**/*.js', //所有js文件
                     dest: 'public/build/assets/scripts/' //输出到此目录下
                 }]
-            },
-            outerJs: { //按原文件结构压缩js文件夹内所有JS文件
-                options: {
-                    mangle: false, //不混淆变量名
-                    preserveComments: 'some', //不删除注释，还可以为 false（删除全部注释），some（保留@preserve @license @cc_on等注释）
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'public/assets/outer/js/', //js目录下
-                    src: '*.js', //所有js文件
-                    dest: 'public/build/outer/js/' //输出到此目录下
-                }]
-            },
+            }
         }, // js uglify
 
         //concat: {
